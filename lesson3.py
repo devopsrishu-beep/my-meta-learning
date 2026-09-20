4a1b113b1a1c7fc18e12dfce4e68667754853d13
rishu <devopsrishu@gmail.com>
2026-09-20 11:54:33 +0000

    lesson3 - vaiables and f-string done

---
+++ lesson3.py
@@ -0,0 +1,7 @@
+name = "rishu"
+age = 22
+goal = "cloud engineer"
+city = "varanasi"
+print(f"my name is {name} and i am from {city} and my goal is {goal}")
+print(age+5) # 5 sal baad kitana age hogi
+print(f"5 saal baad meri age {age+5} hogi aur mai {city} se {goal} banunga")
